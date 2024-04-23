4f0f3ecdce1f7d31f802cea81ec967d69730dac4
Aliza Khan <160646235+AlizaKhnn@users.noreply.github.com>
2024-04-23 14:38:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// unary operator
+var a = 5;
+var b = 2;
+var c;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+console.log(c);
---
+++ main.ts
@@ -0,0 +1,6 @@
+// unary operator
+let a:  number = 5;
+let b: number = 2;
+let c: number;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+console.log(c)
